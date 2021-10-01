99494f835163c96955a7349f1cc79e589ae7ac17
hyunji <b4399@naver.com>
2021-10-01 12:48:34 +0900

    Delete redundant files

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -10,10 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import environ
-import os
 from pathlib import Path
-
+import os
+import environ
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # reading .env file
 environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.evn')
+    env_file=os.path.join(BASE_DIR, '.env')
 )
 
 # Quick-start development settings - unsuitable for production
