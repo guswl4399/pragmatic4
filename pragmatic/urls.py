ec07e0e180fdf1e2e87444bdaf3efa29a452e073
guswl4399 <b4399@naver.com>
2021-11-21 20:21:05 +0900

    retry8

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -19,8 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
-
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
