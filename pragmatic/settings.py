3058504d30a3e4c7a5ce5edd060cc1628ff7a56c
guswl4399 <b4399@naver.com>
2021-11-21 19:32:29 +0900

    retry2

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['141.164.36.53']
 
 
 # Application definition
